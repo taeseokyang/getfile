767f051248fa9691d9bfb8f65b87b36847064b24
taeseokyang <ts.yang.0123@gmail.com>
2024-06-28 08:54:15 +0100

    love

--- gps.py
+++ gps.py
@@ -3,7 +3,7 @@ from pyubx2 import UBXReader
 
 stream = serial.Serial("/dev/ttyAMA2", baudrate=9600, timeout=50)
 ubr = UBXReader(stream)
-gf =  open("gps.txt", "a")
+gf =  open("gps2.txt", "a")
 
 
 while True:
